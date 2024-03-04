3733a5b905f1e50e0689d274c9ccad4882fec5a9
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-03-04 13:08:47 +0100

    start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,25 +3,11 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>
-                Laravel Start 1
-            </h1>
+<h1>
+    Laravel Start 1
+</h1>
 
-            <h2>
-                Ciao {{ $firstName }} {{ $lastName }}
-            </h2>
-
-            <div>
-                <img class="gatto-img" src="{{ Vite::asset('resources/img/gatto.webp') }}" alt="Gatto">
-            </div>
-
-            <div class="gatto-bg">
-                QUESTO DIV AVRA' COME SFONDO IL GATTO
-            </div>
-        </div>
-    </div>
-</div>
+<h2>
+    Ciao {{ $firstName }} {{ $lastName }}
+</h2>
 @endsection
